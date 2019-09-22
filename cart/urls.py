a256ac75bed364faf9b18407b10bc2e9c287f45e
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-22 12:01:38 +0100

    Fixed issue with cart urls

--- cart/urls.py
+++ cart/urls.py
@@ -3,6 +3,6 @@ from .views import view_cart, add_to_cart, adjust_cart
 
 urlpatterns = [
     path('', view_cart, name='view_cart'),
-    path('add/<int:id>/', add_to_cart, name='add_to_cart'),
-    path('adjust/<int:id>/', adjust_cart, name='adjust_cart'),
+    path('add/(?P<id>[0-9]+)/$', add_to_cart, name='add_to_cart'),
+    path('adjust/(?P<id>[0-9]+)/$', adjust_cart, name='adjust_cart'),
 ]
