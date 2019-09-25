12c709925ce00e72a007ec205e4c13e68b47524d
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-25 21:19:20 +0100

    Fixed issue with running locally

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-# env does not get used once deployed to Heroku
-# import env
 import dj_database_url
+
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
